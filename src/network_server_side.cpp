e928eabcd00798b1e3e2c243a20702d96d31aeab
sigma31 <pranetsh@bu.edu>
2024-11-30 21:57:49 -0500

    Fixed compile errors with network side code

--- src/network_server_side.cpp
+++ src/network_server_side.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <vector>
+#include <string>
+#include "cli_funcs.h"
 
-class StorageServer {
-public:
-    StorageServer(int port) : acceptor(io_service, tcp::endpoint(tcp::v4(), port)) {
-        start_accept();
-    }
-
-    //Function to start listening for a connection on the specified port
-    void start_accept() {
-        auto new_session = std::make_shared<Session>(io_service);
-        acceptor.async_accept(new_session->socket(),
-                              [this, new_session](boost::system::error_code ec) {
-                                  if (!ec) new_session->start();
-                                  start_accept();
-                              });
-    }
-
-    void run() { io_service.run(); }
-
-private:
-    boost::asio::io_service io_service;
-    boost::asio::ip::tcp::acceptor acceptor;
-};
+using namespace std;
 
 class Session : public std::enable_shared_from_this<Session> {
 public:
-    explicit Session(boost::asio::io_service &io_service) : socket(io_service) {}
+    explicit Session(boost::asio::io_service &io_service) : _socket(io_service) {}
 
-    boost::asio::ip::tcp::socket &socket() { return socket; }
+    boost::asio::ip::tcp::socket &socket() { return _socket; }
 
     void start() {
-        boost::asio::async_read_until(socket, buffer, '\n',
+        boost::asio::async_read_until(_socket, buffer, '\n',
                                       [this](boost::system::error_code ec, std::size_t) {
                                           if (!ec) handle_request();
                                       });
@@ -57,20 +39,42 @@ public:
 
         // Process the command with CLI parser
         try {
-            CLI_parser(arguments);
+            CLI_parser(arguments); 
         } catch (const string &error) {
             // Send error back to client
             string response = error + "\n";
-            boost::asio::write(socket, boost::asio::buffer(response));
+            boost::asio::write(_socket, boost::asio::buffer(response));
             return;
         }
 
         // Send success message back to client
         string response = "Command executed successfully\n";
-        boost::asio::write(socket, boost::asio::buffer(response));
+        boost::asio::write(_socket, boost::asio::buffer(response));
     }
 
 private:
-    boost::asio::ip::tcp::socket socket;
+    boost::asio::ip::tcp::socket _socket; 
     boost::asio::streambuf buffer;
+};
+
+class StorageServer {
+public:
+    StorageServer(int port) : acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {
+        start_accept();
+    }
+
+    void start_accept() {
+        auto new_session = std::make_shared<Session>(io_service);
+        acceptor.async_accept(new_session->socket(),
+                              [this, new_session](boost::system::error_code ec) {
+                                  if (!ec) new_session->start();
+                                  start_accept();
+                              });
+    }
+
+    void run() { io_service.run(); }
+
+private:
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::acceptor acceptor;
 };
